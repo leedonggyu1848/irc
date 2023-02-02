6ce039f5f664bc37176dc76ae6d609dfb89a7050
donggyu <leedong1848@gmail.com>
2023-02-02 18:20:45 +0900

    test

--- srcs/utils/split.cpp
+++ srcs/utils/split.cpp
@@ -11,7 +11,7 @@ std::vector<std::string>	split(const std::string& str, const std::string& delimi
     while ((pos_end = str.find(delimiter, pos_start)) != std::string::npos) {
         token = str.substr(pos_start, pos_end - pos_start);
         pos_start = pos_end + delim_len;
-        if (token[token.size() -1] == '\r') token.erase(token.size() - 1);
+        if (token[token.size() -1] == '\r') token.pop_back();
         if (token.length() != 0) res.push_back(token);
     }
     token = str.substr(pos_start);
